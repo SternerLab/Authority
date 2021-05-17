dddc0430826289bbda583357c30282f96deac96e
Manuha Vancha <manuvancha@manuhas-air.3500.dhcp.asu.edu>
2021-05-17 13:22:55 -0700

    update mesh terms extraction script

--- r_table/parser/mesh_id_parser/fetch_mesh_terms.sh
+++ r_table/parser/mesh_id_parser/fetch_mesh_terms.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 web_api_examples_path="SKR_Web_API_V2_4/examples"
-input_path="../../../results/mesh/*.txt"
-output_path="../../../results/mesh/output"
+input_path="../../../results/mesh/"
+output_path="../../../results/mesh/output/"
 cd $web_api_examples_path
 
+export UTS_API_KEY="951d27ba-b8b0-4bf6-9d3d-009f45df399c"
 mkdir -p -- $output_path
 
-javac -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.1.1.jar:../lib/httpclient-cache-4.1.1.jar:../lib/httpcore-nio-4.1.jar:../lib/httpclient-4.1.1.jar:../lib/httpcore-4.1.jar:../lib/httpmime-4.1.1.jar -d ../classes GenericBatch.java
+javac -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.2.jar:../lib/httpclient-cache-4.5.13.jar:../lib/httpclient-4.5.13.jar:../lib/httpcore-4.4.13.jar:../lib/httpmime-4.5.13.jar -d ../classes GenericBatch.java
+for filename in "$input_path"*; do
+    echo "$filename"
+    fname=`basename $filename`
+    output_file_name="${output_path}${fname}"
+    echo $fname
+    echo $output_file_name
+    java -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.2.jar:../lib/httpclient-cache-4.5.13.jar:../lib/httpclient-4.5.13.jar:../lib/httpcore-4.4.13.jar:../lib/httpmime-4.5.13.jar GenericBatch $filename > $output_file_name
 
-for filename in $input_path; do
-    echo $filename
-    # (java -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.1.1.jar:../lib/httpclient-cache-4.1.1.jar:../lib/httpcore-nio-4.1.jar:../lib/httpclient-4.1.1.jar:../lib/httpcore-4.1.jar:../lib/httpmime-4.1.1.jar GenericBatch) > %output_folder_path%"\%%~ni.txt"  
+    # java -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.2.jar:../lib/httpclient-cache-4.5.13.jar:../lib/httpclient-4.5.13.jar:../lib/httpcore-4.4.13.jar:../lib/httpmime-4.5.13.jar GenericBatch $filename > ${output_folder_path}${filename}
+    # (java -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.1.1.jar:../lib/httpclient-cache-4.1.1.jar:../lib/httpcore-nio-4.1.jar:../lib/httpclient-4.1.1.jar:../lib/httpcore-4.1.jar:../lib/httpmime-4.1.1.jar GenericBatch $filename) > ${output_folder_path}${filename}"  
 done
