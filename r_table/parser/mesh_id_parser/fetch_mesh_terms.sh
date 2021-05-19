036419cc2223e06fa807dcc144d797ed801ce674
[ Manuha Vancha - Decision Theater - Graduate Student - bsterne1 ] <mvancha@login.agave.rc.asu.edu>
2021-05-19 16:05:56 -0700

    update git ignore and mesh terms parser

--- r_table/parser/mesh_id_parser/fetch_mesh_terms.sh
+++ r_table/parser/mesh_id_parser/fetch_mesh_terms.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 web_api_examples_path="SKR_Web_API_V2_4/examples"
-input_path="../../../results/mesh/"
+input_path="../../../results/mesh/[j-z]*.txt"
 output_path="../../../results/mesh/output/"
 cd $web_api_examples_path
 
-export UTS_API_KEY="951d27ba-b8b0-4bf6-9d3d-009f45df399c"
+export UTS_API_KEY=""
 mkdir -p -- $output_path
 
 javac -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.2.jar:../lib/httpclient-cache-4.5.13.jar:../lib/httpclient-4.5.13.jar:../lib/httpcore-4.4.13.jar:../lib/httpmime-4.5.13.jar -d ../classes GenericBatch.java
@@ -14,8 +14,8 @@ for filename in "$input_path"*; do
     output_file_name="${output_path}${fname}"
     echo $fname
     echo $output_file_name
-    java -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.2.jar:../lib/httpclient-cache-4.5.13.jar:../lib/httpclient-4.5.13.jar:../lib/httpcore-4.4.13.jar:../lib/httpmime-4.5.13.jar GenericBatch $filename > $output_file_name
-
-    # java -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.2.jar:../lib/httpclient-cache-4.5.13.jar:../lib/httpclient-4.5.13.jar:../lib/httpcore-4.4.13.jar:../lib/httpmime-4.5.13.jar GenericBatch $filename > ${output_folder_path}${filename}
-    # (java -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.1.1.jar:../lib/httpclient-cache-4.1.1.jar:../lib/httpcore-nio-4.1.jar:../lib/httpclient-4.1.1.jar:../lib/httpcore-4.1.jar:../lib/httpmime-4.1.1.jar GenericBatch $filename) > ${output_folder_path}${filename}"  
+#     python "../..mesh_abstracts_preprocessor.py" --file $filename
+#     java -cp ../classes:../lib/skrAPI.jar:../lib/commons-logging-1.2.jar:../lib/httpclient-cache-4.5.13.jar:../lib/httpclient-4.5.13.jar:../lib/httpcore-4.4.13.jar:../lib/httpmime-4.5.13.jar GenericBatch $filename > $output_file_name
+    
+     
 done
