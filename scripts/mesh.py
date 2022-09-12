9034ff345576c96b64ba2ec49a8f2755f70a4b7b
LSaldyt <lucassaldyt@gmail.com>
2022-09-12 12:27:58 -0700

    Verify/test MeSH in batch mode of 16

--- scripts/mesh.py
+++ scripts/mesh.py
@@ -8,6 +8,7 @@ from itertools import islice
 
 from authority.process.process import remove_stop_words
 import unicodedata
+from collections import defaultdict
 
 mesh_format = '''UI  - {article_id}
 TI  - {title}
@@ -24,8 +25,7 @@ def run():
     # See https://github.com/lhncbc/skr_web_python_api/blob/main/examples/generic_batch.py
     sub = skr_web_api.Submission(credentials['email'], credentials['api_key'])
     # See https://lhncbc.nlm.nih.gov/ii/tools/MTI/help_info.html
-    # sub.init_generic_batch('MTI', '-default_MTI -opt1_DCMS -E --singleLineInput -display_simple3')
-    sub.init_generic_batch('MTI', '-opt1L_DCMS -E') # The default options
+    sub.init_generic_batch('MTI', '-opt1L_DCMS -E') # The default options used previously
 
     client = MongoClient('localhost', 27017)
     jstor_database = client.jstor_database
@@ -36,31 +36,34 @@ def run():
         batch = islice(article_cursor, batch_size)
         abstracts = []
         for article in batch:
-            abstract = fix_unicode(article['abstract'])
-            title    = fix_unicode(article['title'])
-            mongo_id = article["_id"]
             abstracts.append(
-                (mesh_format.format(article_id=mongo_id, title=title, abstract=abstract),
-                 mongo_id))
+                (mesh_format.format(
+                    article_id=article["_id"],
+                    title=fix_unicode(article['title']),
+                    abstract=fix_unicode(article['abstract'])),
+                 article['_id']))
+        if len(abstracts) == 0:
+            break
         sub.set_batch_file('USERINPUT', '.\n\n'.join(a for a, _ in abstracts))
-        pprint(sub.files)
         print(f'Submitting article to SKR API!', flush=True)
         response = sub.submit()
         assert response.status_code == 200, f'Bad response, {response}'
         print(f'Received {response.status_code}!', flush=True)
 
-        mesh_output = []
+        mesh_output = defaultdict(set)
         lines = response.content.decode('utf-8', errors='replace').split('\n')
         print('all lines:')
         pprint(lines)
         for i, line in enumerate(lines):
-            print(i, line)
             if len(line.strip()) == 0 or '|' not in line:
                 continue
+            print(i, line)
             id_cell, *cells = line.split('|')
-            mesh_output.extend(remove_stop_words(cells, 'mesh'))
-        print('Updating', mongo_id)
-        pprint(mesh_output)
-        1/0
-        articles.update_one({'_id' : ObjectId(mongo_id)}, {'$set' : {'mesh' : list(set(mesh_output))}})
+            mesh_output[id_cell].update(remove_stop_words(cells, 'mesh'))
+        for mongo_id, words in mesh_output.items():
+            print('Updating', mongo_id)
+            pprint(words)
+            articles.update_one(
+                {'_id' : ObjectId(mongo_id)},
+                {'$set' : {'mesh' : list(words)}})
         print('Update finished!', flush=True)
