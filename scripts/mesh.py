c9e8d29479ffa3455ba9439e6cfbe8327a584cb8
LSaldyt <lucassaldyt@gmail.com>
2022-09-12 12:02:35 -0700

    Update NIH/SKR formatting so that mesh can be done in batch mode!

--- scripts/mesh.py
+++ scripts/mesh.py
@@ -9,20 +9,41 @@ from itertools import islice
 from authority.process.process import remove_stop_words
 import unicodedata
 
+mesh_format = '''UI  - {article_id}
+TI  - {title}
+AB  - {abstract}'''
+
+def fix_unicode(s):
+    s = s.replace('\n', '')
+    s = unicodedata.normalize('NFKD', s).encode('ascii', 'ignore').decode('ascii', 'ignore')
+    return s
+
 def run():
     with open('umls_credentials.json', 'r') as infile:
         credentials = json.load(infile)
+    # See https://github.com/lhncbc/skr_web_python_api/blob/main/examples/generic_batch.py
     sub = skr_web_api.Submission(credentials['email'], credentials['api_key'])
-    sub.init_generic_batch('MTI', '')
+    # See https://lhncbc.nlm.nih.gov/ii/tools/MTI/help_info.html
+    # sub.init_generic_batch('MTI', '-default_MTI -opt1_DCMS -E --singleLineInput -display_simple3')
+    sub.init_generic_batch('MTI', '-opt1L_DCMS -E') # The default options
 
     client = MongoClient('localhost', 27017)
     jstor_database = client.jstor_database
     articles       = jstor_database.articles
-    for article in articles.find(): # TODO add a check to see if mesh info is already present, then skip if it is
-        abstract = article['abstract'].replace('\n', '')
-        abstract = unicodedata.normalize('NFKD', abstract).encode('ascii', 'ignore').decode('ascii', 'ignore') # fun
-        mongo_id = article["_id"]
-        sub.set_batch_file('USERINPUT', f'{abstract}.')
+    article_cursor = articles.find()
+    batch_size = 4
+    while True:
+        batch = islice(article_cursor, batch_size)
+        abstracts = []
+        for article in batch:
+            abstract = fix_unicode(article['abstract'])
+            title    = fix_unicode(article['title'])
+            mongo_id = article["_id"]
+            abstracts.append(
+                (mesh_format.format(article_id=mongo_id, title=title, abstract=abstract),
+                 mongo_id))
+        sub.set_batch_file('USERINPUT', '.\n\n'.join(a for a, _ in abstracts))
+        pprint(sub.files)
         print(f'Submitting article to SKR API!', flush=True)
         response = sub.submit()
         assert response.status_code == 200, f'Bad response, {response}'
@@ -40,5 +61,6 @@ def run():
             mesh_output.extend(remove_stop_words(cells, 'mesh'))
         print('Updating', mongo_id)
         pprint(mesh_output)
+        1/0
         articles.update_one({'_id' : ObjectId(mongo_id)}, {'$set' : {'mesh' : list(set(mesh_output))}})
         print('Update finished!', flush=True)
