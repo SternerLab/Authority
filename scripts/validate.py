4104eb11d5b5ffc453f0c0606e5e9b0eb9760224
LSaldyt <lucassaldyt@gmail.com>
2022-09-20 10:58:51 -0700

    Initial test-set statistics

--- scripts/validate.py
+++ scripts/validate.py
@@ -27,12 +27,34 @@ def run():
 
     xi_ratios, interpolated = get_r_table_data(r_table)
 
+    # for bhl_cluster in bhl.find():
+    #     key = bhl_cluster['author']['key']
+    #     print(key)
+    #     for pair in pairs['match'].find({'pair' : {'authors' : {'key' : key}}}):
+    #         print('bhl cluster')
+    #         pprint(bhl_cluster)
+    #         print('pair')
+    #         pprint(pair)
+    # 1/0
+
+
+    n = 1000
     match_prior = 1/11 # for testing, bad :)
-    for pair in pairs['block'].find():
-        try:
-            pprint(pair)
-            compared = compare_pair(pair, articles)
-            features = compared['features']
-            print(infer_from_feature(features, interpolated, xi_ratios, match_prior))
-        except KeyError:
-            pass
+    for group in ('match', 'non_match', 'block'):
+        print(group)
+        ps = []
+        i = 0
+        for pair in pairs[group].find():
+            if i == n:
+                break
+            try:
+                # pprint(pair)
+                compared = compare_pair(pair, articles)
+                features = compared['features']
+                p = infer_from_feature(features, interpolated, xi_ratios, match_prior)
+                # print(p)
+                ps.append(p)
+            except KeyError:
+                pass
+            i += 1
+        print(group, sum(ps) / n)
