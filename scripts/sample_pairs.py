dfdaed3df8bd838a1251088da07eeb64abd7f658
LSaldyt <lucassaldyt@gmail.com>
2022-10-03 11:10:54 -0700

    Add threading to sample pairs with independent progress bars

--- scripts/sample_pairs.py
+++ scripts/sample_pairs.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
 import pymongo
 from rich.pretty import pprint
-from rich.progress import track
+from rich.progress import track, Progress
 from rich import print
 from bson.son import SON
+from functools import partial
 import itertools
+from concurrent.futures import ThreadPoolExecutor as Pool
+from threading import Lock
+progress_lock = Lock()
 
 # See this reference on MongoDB aggregation:
 # https://pymongo.readthedocs.io/en/stable/examples/aggregation.html
@@ -18,10 +22,43 @@ def sample_pairs(group_doc):
 
 def sample_grouped_pairs(database, ref_key):
     total = database[ref_key].count_documents({})
-    for group_doc in track(database[ref_key].find(), total=total):
+    for group_doc in database[ref_key].find():
         group_id = group_doc['_id']
         n  = group_doc.get('count', None)
         yield group_id, n, sample_pairs(group_doc)
+
+def sample_for_ref_key(ref_key, progress, reference_sets, reference_sets_group_lookup,
+                       reference_sets_pairs, every=10000):
+    if ref_key == 'non_match':
+        limit = 11295361 # Arbitrary but limited
+    else:
+        limit = float('inf')
+
+    task_name = f'Sampling pairs from {ref_key}'
+    total = reference_sets[ref_key].count_documents({})
+    with progress_lock:
+        task  = progress.add_task(task_name, total=total)
+
+    inserted = 0
+    threshold = inserted + every
+    for group_id, n, grouped_pairs in sample_grouped_pairs(reference_sets, ref_key):
+        try:
+            result = reference_sets_pairs[ref_key].insert_many(grouped_pairs)
+            inserted += len(result.inserted_ids)
+            reference_sets_group_lookup[ref_key].insert_one(
+                    dict(group_id=group_id, pair_ids=result.inserted_ids,
+                         n=n))
+        except pymongo.errors.InvalidOperation:
+            pass # Only one element in group, cannot make pairs
+        if inserted > limit:
+            break
+        if inserted > threshold:
+            print(f'{inserted:20} in {ref_key:10} ...')
+            threshold = inserted + every
+        with progress_lock:
+            progress.update(task, advance=1)
+
+    return inserted
 
 def run():
     ''' Use the reference sets to create pairs of articles in a new database '''
@@ -37,27 +74,17 @@ def run():
 
     total  = articles.count_documents({})
 
+    threads = 3
     ref_keys = ('block', 'match', 'non_match')
-    ref_keys = ('non_match',)
 
-    for ref_key in ref_keys:
-        print(f'Sampling pairs from {ref_key}', flush=True)
-        if ref_key == 'non_match':
-            limit = 11295361 # Arbitrary but limited
-        else:
-            limit = float('inf')
-        inserted = 0
-        for group_id, n, grouped_pairs in sample_grouped_pairs(reference_sets, ref_key):
-            try:
-                result = reference_sets_pairs[ref_key].insert_many(grouped_pairs)
-                inserted += len(result.inserted_ids)
-                reference_sets_group_lookup[ref_key].insert_one(
-                        dict(group_id=group_id, pair_ids=result.inserted_ids,
-                             n=n))
-            except pymongo.errors.InvalidOperation:
-                pass # Only one element in group, cannot make pairs
-            if inserted > limit:
-                break
+    with Progress() as progress:
+        with Pool(max_workers=threads) as pool:
+            f = partial(sample_for_ref_key, progress=progress,
+                        reference_sets=reference_sets,
+                        reference_sets_pairs=reference_sets_pairs,
+                        reference_sets_group_lookup=reference_sets_group_lookup)
+            print(list(pool.map(f, ref_keys)))
 
     for ref_key in ref_keys:
         print(ref_key, reference_sets_pairs[ref_key].count_documents({}))
+
