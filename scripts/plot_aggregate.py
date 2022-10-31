05c493464e1791c75345ac73fd273fe202a8b21b
LSaldyt <lucassaldyt@gmail.com>
2022-10-31 13:10:39 -0700

    Remove old plots, update plotting

--- scripts/plot_aggregate.py
+++ scripts/plot_aggregate.py
@@ -1,23 +1,25 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy  as np
 
 from rich.progress import track
 
 def run():
-    val_df = pd.read_csv('authority_validation_metrics_october31_backup.csv')
+    # val_df = pd.read_csv('authority_validation_metrics_october31_backup.csv')
+    val_df = pd.read_csv('authority_validation_metrics_backup.csv')
     val_df.fillna(0, inplace=True)
     print(val_df)
     print(val_df.describe())
 
-    for metric in ('lumping', 'splitting', 'accuracy'):
+    metrics = ('lumping', 'splitting', 'accuracy')
+    for metric in metrics:
         fig = sns.displot(val_df, x=metric)
         fig.set_axis_labels(metric.title(), 'Frequency')
         fig.savefig(f'plots/{metric}_frequency.png')
         plt.show()
         plt.clf()
 
-    val_df['frequency'] = 0
     names_df = pd.read_csv('names.csv')
     print(names_df)
     sns.displot(names_df, x='count', log_scale=True)
@@ -25,17 +27,25 @@ def run():
     plt.show()
     plt.clf()
 
+    lookup = dict()
     for tup in track(names_df.itertuples(), total=names_df.shape[0]):
-        queried = val_df[val_df.name == tup.name]
-        if queried.empty:
-            continue
-        queried['frequency'] = tup.count
+        lookup[tup.name] = tup.count
+
+    frequencies = np.zeros(val_df.shape[0])
+    for i, tup in track(enumerate(val_df.itertuples()), total=val_df.shape[0]):
+        frequencies[i] = lookup.get(tup.name, 0)
+    val_df['frequency'] = frequencies
     val_df.to_csv('composite.csv')
 
     plt.clf()
     composite = pd.read_csv('composite.csv')
-    print(composite)
-    sns.scatterplot(composite, x='frequency', y='accuracy')
-    plt.show()
-    plt.clf()
+    print(composite['frequency'].describe())
+    print(composite['frequency'].max())
+    for metric in metrics:
+        ax = sns.scatterplot(composite, x='frequency', y=metric)
+        ax.set_xlabel('Name Frequency')
+        ax.set_ylabel(metric.title())
+        plt.savefig(f'plots/{metric}_name_frequency.png')
+        plt.show()
+        plt.clf()
 
