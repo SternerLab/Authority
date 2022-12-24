51c1dadb4f59310d01ecb2b501a060abbe8e73f4
LSaldyt <lucassaldyt@gmail.com>
2022-12-23 20:21:39 -0700

    Improve OrCID logs/threading/rate limiting

--- resolution/validation/orcid.py
+++ resolution/validation/orcid.py
@@ -35,6 +35,7 @@ class OrcidScraper:
         self.token = self.api.get_search_token_from_orcid()
         self.max_threads = max_threads
         self.max_rate = max_rate
+        self.thread_max_rate = max_rate / max_threads
         self.buffer = buffer
 
     def yield_works(self, works):
@@ -52,7 +53,7 @@ class OrcidScraper:
                     pass # DOIs are not critical, so long as we get a few
                 yield title, doi
 
-    def yield_search(self, query, key='works', desc='', max_rate=3.0):
+    def yield_search(self, query, key='works', desc='', max_rate=4.0):
         start = time()
         n_requests = 1
         results = self.api.search(query, access_token=self.token)
@@ -61,12 +62,14 @@ class OrcidScraper:
             try:
                 doc = self.api.read_record_public(orcid_id, key, self.token)
                 n_requests += 1
-                current = time()
-                rate = n_requests / (current - start)
+                duration = time() - start
+                rate = n_requests / duration
                 if rate >= max_rate:
-                    log.info(f'')
-                    sleep(1)
-                log.debug(f'OrCID requests/second: {rate}')
+                    # log.info(f'Internal thread rate limiting! {rate} >= max_rate')
+                    fix = (n_requests / max_rate) - duration + 0.1
+                    # log.info(f'Sleeping {fix}s')
+                    sleep(fix)
+                # log.debug(f'OrCID requests/second: {rate}')
             except Exception as e:
                 print(f'{type(Exception).__name__}: {e}')
                 continue
@@ -76,7 +79,8 @@ class OrcidScraper:
         i, query = query
         log.info(f'Thread {i} query: {query}')
         if query not in lookup: # For efficiency, assuming OrCID is definitive
-            for orcid_id, works, n_requests in self.yield_search(query, key='works', desc=f'{query} ({i})'):
+            for orcid_id, works, n_requests in self.yield_search(query, key='works',
+                                                                 desc=f'{query} ({i})',                                                          max_rate=self.thread_max_rate):
                 cluster = []
                 full    = []
                 for title, doi in self.yield_works(works):
@@ -118,8 +122,9 @@ class OrcidScraper:
             log.info(f'Resolution Rate: {resolve_rate} articles per minute')
             log.info(f'Requests Rate: {requests_rate} articles per minute')
             if requests_rate >= self.max_rate:
-                log.warning(f'OrCID rate exceeded, sleeping 1s')
-                sleep((l / self.max_rate) - duration + self.buffer)
+                fix = (l / self.max_rate) - duration + self.buffer
+                log.warning(f'OrCID rate exceeded, sleeping {fix}s')
+                sleep(fix)
 
         return lookup, [list(c) for c in clusters.values()]
 
